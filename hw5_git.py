f23cb0024bfa0a3c3fcfe4607bb0b1f170b4d7d9
Karley Thurston <thurstkd@umich.edu>
2021-03-15 22:06:33 -0400

    Updating hw5

--- hw5_git.py
+++ hw5_git.py
@@ -1,2 +1,8 @@
-for b in ['b','i','n','g','o']:
+for b in ['b','i','n','g','o', '! ']:
     print(b)
+
+for i in range(3):
+    for b in ['b','i','n','g','o''! ']:
+        print(b)
+
+print('and bingo was his name-o!')
